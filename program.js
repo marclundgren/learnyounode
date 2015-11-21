f803e1f7672d13fc12419a18b951e63826277b89
Marc Lundgren <marclundgren2.0@gmail.com>
2015-11-21 13:55:45 -0800

    lesson 2

--- program.js
+++ program.js
@@ -1,1 +1,8 @@
-console.log('HELLO WORLD');
+'use strict';
+let args = process.argv.slice(2);
+
+let result = args.reduce((previousValue, currentValue) => {
+  return Number(previousValue) + Number(currentValue);
+});
+
+console.log(result);
